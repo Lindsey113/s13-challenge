11b6d1239a8be88355938a84b4f3ec470fb9c1d9
Lindsey <lindsey_mccluskey@yahoo.ca>
2024-10-31 12:19:40 -0700

    ALMOST DONE

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -36,4 +36,55 @@ router.get('/api/actions/:id', (req, res, next) => {
 
 })
 
+router.post('/api/actions', validateBody, async (req, res, next) => {
+    try {
+        const { 
+            project_id, 
+            description, 
+            notes, 
+            completed } = req.body
+        const createAction = await Actions.insert({
+            project_id, 
+            description, 
+            notes, 
+            completed
+        })
+        if(createAction){
+            res.status(201).json(createAction)
+        } else {
+            res.status(500).json({
+                message: "Failed"
+            })
+        }
+    } catch(err){
+        next(err)
+    }
+})
+
+router.put('/api/actions/:id', validateId, validateBody, (req, res, next) => {
+    const changes = req.body
+    const { id } = req.params
+    Actions.update(id, changes)
+        .then(updateAction => {
+            if(updateAction){
+                res.status(200).json(updateAction)
+                
+            } else {
+                res.status(404).json({
+                    message: "aaaaaaaaa"
+                })
+            }
+        })
+        .catch(next)
+})
+
+router.delete('/api/actions/:id', validateId, async (req, res, next) => {
+    try {
+        const deleted = await Actions.remove(req.params.id)
+        res.json(deleted)
+    } catch(err) {
+        next(err)
+    }
+})
+
 module.exports = router
